3446a24c1d4bf2eabdc4dad0b7313cb60ee820e8
Junio Henrique (Kokada) <Resource.Junio.Oliveira@br.experian.com>
2019-04-03 09:35:32 -0300

    add circleci integration

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
+// Protractor configuration file, see link for more information
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+
+const { SpecReporter } = require('jasmine-spec-reporter');
+
 exports.config = {
-	seleniumAddress: 'http://localhost:4444/wd/hub',
-	specs: ['index_spec.js'],
+	specs: [
+		'./spec/*.spec.js'
+	],
 	multiCapabilities: [
 		{
 			'browserName': 'chrome',
@@ -8,5 +14,19 @@ exports.config = {
 				args: [ "--headless", "--disable-gpu"]
 			}
 		}
-	]
+	],
+	directConnect: true,
+	seleniumAddress: 'http://localhost:4444/wd/hub',
+	baseUrl: 'https://www.pokemon.com/us/pokedex/',
+	framework: 'jasmine',
+	jasmineNodeOpts: {
+		showColors: true,
+		print: function () { }
+	},
+	onPrepare() {
+		browser. waitForAngularEnabled(false);
+		browser.manage().timeouts().implicitlyWait(5000);
+		browser.driver.manage().window().setSize(1280, 1024);
+		jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+	}
 };
